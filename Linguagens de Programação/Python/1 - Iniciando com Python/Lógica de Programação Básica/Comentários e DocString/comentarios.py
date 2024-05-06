9f11bc370f8d1077578fcb376af19e7053b04299
David Eugenio F Pereira <154105663+DataWithDavid@users.noreply.github.com>
2024-05-06 20:00:56 -0300

    Add files via upload

---
+++ Linguagens de Programação/Python/1 - Iniciando com Python/Lógica de Programação Básica/Comentários e DocString/comentarios.py
@@ -0,0 +1,4 @@
+# Comentários de Código são linhas não executáveis, assim que o python for interpretar o arquivo, ele irá ignorar está linha
+
+#Comentário são muito utilizados para saber o que a linha ou o bloco de código está fazendo.
+print('Este código vai ser exibido na tela (será enviado para o output)')
---
+++ Linguagens de Programação/Python/1 - Iniciando com Python/Lógica de Programação Básica/Comentários e DocString/docstring.py
@@ -0,0 +1,19 @@
+# O python não pula estas linhas de códigos, ela é chamada como multilinhas.
+
+"""
+Isto não é um comentário
+Porém podemos utilizar para fazer notas
+Sobre o código
+
+
+- O interpretador do Python Lê
+- Chamado de Docstring
+- Pode ser usado tanto com aspas duplas ("") ou aspas simples ('')
+- Muito usado para anotar os passos utilizados na escrita dos códigos
+- Pode ter multiplas linhas
+- O Começo e o fim devem ser iguais, se começou com aspas simples tem que terminar com aspas simples
+- O interpretador guarda essas linhas na memória
+
+"""
+
+print('Testando o docstring')
